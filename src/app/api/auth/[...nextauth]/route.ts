05253b4a2a5de96e3735e5a9f0c7d5da296a22e4
Maya Reich <mayareich0606@gmail.com>
2026-07-30 23:52:07 +0200

    route handling

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,2 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-
-const authOptions = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-});
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POSt };
+import { handlers } from '@/auth';
+export const { GET, POST } = handlers;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { auth, signIn, signOut } from '@/auth';
 
-const LoginPage = () => {
-  const { data: session, status } = useSession();
+export default async function LoginPage() {
+  const session = await auth();
 
-  if (status === 'authenticated') {
+  if (session) {
     return (
       <>
         <p>Welcome, {session.user?.email}</p>
-        <button onClick={() => signOut()}> Sign out</button>
+        {/* Server Action for Sign Out */}
+        <form
+          action={async () => {
+            'use server';
+            await signOut({ redirectTo: '/login' });
+          }}
+        >
+          <button type="submit">Sign out</button>
+        </form>
       </>
     );
   }
+
   return (
     <>
       <p>You are not signed in</p>
-      <button onClick={() => signIn()}>Sign in with Google</button>
+      {/* Server Action for Google Sign In */}
+      <form
+        action={async () => {
+          'use server';
+          await signIn('google', { redirectTo: '/dashboard' });
+        }}
+      >
+        <button type="submit">Sign in with Google</button>
+      </form>
     </>
   );
-};
-
-export default LoginPage;
+}
